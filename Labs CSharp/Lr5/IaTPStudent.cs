1be8cfa0750dc91be161cb8cdb73fb0ec24777e0
AshRaven521 <56455248+AshRaven521@users.noreply.github.com>
2020-04-07 22:29:35 +0300

    lab 5 fixed

--- Labs CSharp/Lr5/IaTPStudent.cs
+++ Labs CSharp/Lr5/IaTPStudent.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace university
 {
-    enum Specialtys
+    enum SpecialtyName
     {
         IaTP=1,
         CSaN,
@@ -20,117 +20,42 @@ namespace university
         public int Hours { get; set; }
     }
 
-    class IaTPStudent:Student
+    class CompetenciesPerson:Student
+  
     {
-        public Specialtys  Specialty { get; set; }
+        public SpecialtyName  Specialty { get; set; }
         public string GroupNumber { get; set; }
         public string Qualification { get; set; }
 
-        Discipline dis;
+        public Discipline dis;
 
-        public IaTPStudent():base()
+        public CompetenciesPerson():base()
         {
-            Specialty = Specialtys.IaTP;
+            Specialty = SpecialtyName.IaTP;
             GroupNumber = "753503";
             dis.DisciplineName = "ISP";
             dis.Hours = 120;
             Qualification = "System Engineer";
+            hobbies = new string[2];
+            hobbies[0] = "Rubik's cubes solving";
+            hobbies[1] = "Playing table tennis";
         }
 
         public override string ToString()
         {
-            return base.ToString()+"Specialty: "+ Specialty + ";" + "Group: " + GroupNumber + ";" + "Discipline: " +dis.DisciplineName + ";" + "Hours for input discipline: " + dis.Hours;
-        }
-
-
-        public void SetParametrs()
-        {
-            Console.Write("Enter name: ");
-            Name = Console.ReadLine();
-
-            Console.Write("Enter age: ");
-            Age = ValidateAge();
-
-            Console.Write("Enter height in meters: ");
-            HeightMeters = CheckDouble();
-
-            Console.Write("Enter weight in kilograms: ");
-            WeightKilos = CheckDouble();
-
-            Console.Write("Enter city where he/her was born: ");
-            CityBorn = Console.ReadLine();
-
-            SetId();
-
-            Console.Write("Enter faculty: ");
-            Faculty = Console.ReadLine();
-
-            Console.Write("Enter course: ");
-            Course =CheckInt();
-
-            Console.Write("Enter specialty(1-IaTP, 2-CSaN, 3-ITS, 4-ECS): ");
-            Specialty = (Specialtys)ValidateSpeciality();
-
-            Console.Write("Enter group number: ");
-            GroupNumber = Console.ReadLine();
-
-            Console.Write("Enter qualification: ");
-            Qualification = Console.ReadLine();
-
-            Console.Write("Enter discipline: ");
-            dis.DisciplineName = Console.ReadLine();
-
-            Console.Write("Enter hours for this discipline: ");
-            dis.Hours = CheckInt();
-        }
+            string str = " ";
+            for (int i = 0; i < hobbies.Length; i++)
+            {
+                str = str + hobbies[i] + ";";
+            }
+            return base.ToString() + "Specialty: " + Specialty + ";" + "Group: " + GroupNumber + ";" + "Discipline: " + dis.DisciplineName + ";" + "Hours for input discipline: " + dis.Hours + ";" + "Students hobbies: " + str + "\n";
+        } 
 
         public override void DiplomaType()
         {
             Console.WriteLine("Your qualification is {0}",Qualification);
         }
 
-        public static int ValidateSpeciality()
-        {
-            int result;
-            while (!int.TryParse(Console.ReadLine(), out result) || result < 1 || result > 4)
-            {
-                Console.Write("Incorrect input, please try again: ");
-            }
-                
-            return result;
-        }
-
-        public static int ValidateAge()
-        {
-            int vozrast;
-            while(!int.TryParse(Console.ReadLine(), out vozrast) || vozrast<=15)
-            {
-                Console.Write("Incorrect age input, please try again: ");
-            }
-
-            return vozrast;
-        }
-
-        public static int CheckInt()
-        {
-            int a;
-            while(!int.TryParse(Console.ReadLine(), out a) || a<=0)
-            {
-                Console.Write("Incorrect input, please try again: ");
-            }
-
-            return a;
-        }
-
-        public static double CheckDouble()
-        {
-            double b;
-            while(!double.TryParse(Console.ReadLine(), out b) || b<=0)
-            {
-                Console.Write("Incorrect input, please try again: ");
-            }
-
-            return b;
-        }
+        
     }
 }
--- Labs CSharp/Lr5/MainProg.cs
+++ Labs CSharp/Lr5/MainProg.cs
@@ -8,18 +8,105 @@ namespace university
 {
     class Program
     {
+
+        public static void SetParametrs(CompetenciesPerson obj)
+        {
+            Console.Write("Enter name: ");
+            obj.Name = Console.ReadLine();
+
+            Console.Write("Enter age: ");
+            obj.Age = ValidateAge();
+
+            Console.Write("Enter height in meters: ");
+            obj.HeightMeters = CheckDouble();
+
+            Console.Write("Enter weight in kilograms: ");
+            obj.WeightKilos = CheckDouble();
+
+            Console.Write("Enter city where he/her was born: ");
+            obj.CityBorn = Console.ReadLine();
+
+            obj.SetId();
+
+            Console.Write("Enter faculty: ");
+            obj.Faculty = Console.ReadLine();
+
+            Console.Write("Enter course: ");
+            obj.Course = CheckInt();
+
+            Console.Write("Enter specialty(1-IaTP, 2-CSaN, 3-ITS, 4-ECS): ");
+            obj.Specialty = (SpecialtyName)ValidateSpeciality();
+
+            Console.Write("Enter group number: ");
+            obj.GroupNumber = Console.ReadLine();
+
+            Console.Write("Enter qualification: ");
+            obj.Qualification = Console.ReadLine();
+
+            Console.Write("Enter discipline: ");
+            obj.dis.DisciplineName = Console.ReadLine();
+
+            Console.Write("Enter hours for this discipline: ");
+            obj.dis.Hours = CheckInt();
+        }
+
+        public static int ValidateSpeciality()
+        {
+            int result;
+            while (!int.TryParse(Console.ReadLine(), out result) || result < 1 || result > 4)
+            {
+                Console.Write("Incorrect input, please try again: ");
+            }
+
+            return result;
+        }
+
+        public static int ValidateAge()
+        {
+            int vozrast;
+            while (!int.TryParse(Console.ReadLine(), out vozrast) || vozrast <= 15)
+            {
+                Console.Write("Incorrect age input, please try again: ");
+            }
+
+            return vozrast;
+        }
+
+        public static int CheckInt()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                Console.Write("Incorrect input, please try again: ");
+            }
+
+            return a;
+        }
+
+        public static double CheckDouble()
+        {
+            double b;
+            while (!double.TryParse(Console.ReadLine(), out b) || b <= 0)
+            {
+                Console.Write("Incorrect input, please try again: ");
+            }
+
+            return b;
+        }
+
         static void Main()
         {
-            IaTPStudent std = new IaTPStudent();
+            CompetenciesPerson std = new CompetenciesPerson();
 
             Console.WriteLine("Info about default student : ");
             Console.WriteLine(std);
             Console.WriteLine("\n");
 
-            std.SetParametrs();
+            SetParametrs(std);
             Console.WriteLine();
 
             std.DiplomaType();
+            Console.WriteLine();
             Console.WriteLine(std);
             Console.WriteLine();
 
